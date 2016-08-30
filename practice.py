d83e80ecefd3092b7ac6b487f759c8f9f29f4129
Tim Padjen <tpadjen@gmail.com>
2016-08-30 11:48:15 -0600

    Add practice function problems

---
+++ practice.py
@@ -0,0 +1,94 @@
+# *****************************************************
+#
+# 	Functions
+#
+# 		Write the functions below. Write separate code 
+# 		to test each possible outcome for each function.
+# 		DO NOT print any info to the console within the 
+# 		functions.
+#
+# *****************************************************
+
+
+# sleep_in
+#
+# Students sleep in if it is not a weekday or we're on vacation. 
+#
+# > Parameters
+# ------------ 
+# 	* weekday  - true only if it is a weekday
+# 	* vacation - true only if the student is on vacation
+#
+# TODO - write sleep_in
+
+
+# monkey_trouble
+#
+# We have two monkeys, a and b, and the parameters a_smile and b_smile 
+# indicate if each is smiling. 
+# We are in trouble if they are both smiling or if neither of them is smiling.
+# Return true if we are in trouble.
+#
+# > Parameters 
+# ------------
+# 	* a_smiling   - true only if monkey a is smiling
+# 	* b_smiling   - true only if monkey b is smiling
+#
+# TODO - write monkey_trouble
+
+
+# sum_double
+#
+# Given two int values, return their sum. Unless the two values are the same,
+# then return double their sum. 
+#
+# > Parameters 
+# ------------
+# 	* a - an integer
+# 	* b - an integer
+#
+# TODO - write sum_double
+
+
+# blackjack
+#
+# Given 2 int values greater than 0, return whichever value is nearest to 21 
+# without going over. Return 0 if they both go over.
+#
+# > Parameters 
+# ------------
+# 	* card1 - an integer
+# 	* card2 - an integer
+#
+# TODO - write blackjack (tests first)
+
+
+# n_twice
+#
+# Given a string and an int n, return a string made of the first and last n 
+# chars from the string.
+#
+# Hint | Use string.slice
+# 		 | http://ruby-doc.org/core-2.2.0/String.html#method-i-slice
+#
+# > Parameters 
+# ------------
+# 	* str - a string with length at least n
+# 	* n   - an integer
+#
+# TODO - write n_twice (tests first)
+
+
+# close_far
+#
+# Given three ints, a b c, return true if one of b or c is "close" (differing 
+# from a by at most 1), while the other is "far", differing from both other 
+# values by 2 or more. Note: n.abs computes the absolute value of number n.
+#
+# > Parameters 
+# ------------
+# 	* a - an integer
+# 	* b - an integer
+# 	* c - an integer
+#
+# TODO - write close_far (tests first)
